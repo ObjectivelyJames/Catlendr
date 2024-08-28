acc145e945b79b551dbf0aea66fccbdb91799d4b
James Larcombe <jameslarcombe@gmail.com>
2024-08-28 10:28:22 -0700

    Initial Commit

---
+++ Catlendr/CatlendrApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatlendrApp.swift
+//  Catlendr
+//
+//  Created by James Larcombe on 2024-08-28.
+//
+
+import SwiftUI
+
+@main
+struct CatlendrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
